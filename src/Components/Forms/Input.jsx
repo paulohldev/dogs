71226a6137b429a26567313cd285fb95033ce793
paulohldev <paulo85542080@gmail.com>
2022-12-15 22:24:27 -0300

    useForm

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import styles from './Input.module.css';
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
+  // console.log(error && error);
+  if (error) {
+    console.log(error);
+  }
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input className={styles.input} type={type} id={name} name={name} />
-      <p className={styles.error}>Error</p>
+      <input
+        className={styles.input}
+        type={type}
+        id={name}
+        name={name}
+        onChange={onChange}
+        value={value}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
